68a24f0470b637e14d1edfae0d92bab0ba53dbfb
Alepo2412 <55759742+alepo2412@users.noreply.github.com>
2021-06-09 13:41:41 -0600

    new

--- California/app/helpers/database.php
+++ California/app/helpers/database.php
@@ -16,7 +16,7 @@ class Database
     {
         // Credenciales para establecer la conexión con la base de datos.
         $server = 'localhost';
-        $database = 'dbPrueba';
+        $database = 'dba';
         $username = 'postgres';
         $password = 'newpassword';
 
--- California/views/public/index.php
+++ California/views/public/index.php
@@ -38,58 +38,6 @@ Public_Page::headerTemplate('california');
       </div>
     </div>
     <br><br>
-
-    <!--Noticias-->
-    <div>
-      <h2 class="pad-nav Texto center">Noticias</h2>
-      <br><br>
-    </div>
-    <div class="row">
-      <a href="noticia.php">
-        <div class="col s6 m6 l3">
-          <div class="card alto-card white rad">
-            <div class="card-content black-text">
-              <span class="card-title Titulos">California</span>
-              <p class="Texto">¡La nueva línea de Skateboards 100% autenticas ya está aquí! Estamos orgullosos de poner a rodar nuestras nuevas tablas.</p>
-            </div>            
-          </div>
-        </div>
-      </a>
-      <div class="col s6 m6 l3">          
-        <a href="noticia.php">
-          <div class="card alto-card white z-depth-1 rad">
-            <div class="card-content black-text">
-              <span class="card-title Subtitulos">Vans</span>
-              <p class="Texto">¡Hemos conseguido que VANS entre en nuestra pista! Podrás conseguir sus productos muy pronto en nuestra tienda.</p>
-            </div>            
-          </div>
-        </a>
-      </div>
-      <a href="noticia.php">
-        <div class="col s6 m6 l3">
-          <div class="card alto-card white z-depth-1 rad">
-            <div class="card-content black-text">
-              <span class="card-title Subtitulos">Element</span>
-              <p class="Texto">¡Descuentos! Encuentra productos Element con hasta un 30% de descuento. Tienes hasta el 31 de marzo para buscarlos.</p>
-            </div>            
-          </div>
-        </div>
-      </a>        
-      <a href="noticias.php">
-        <div class="col s6 m6 l3">
-          <div class="card alto-card white z-depth-1 rad center">
-            <div class="card-content black-text">              
-              <p class="Texto">Ver todas las noticias.</p>
-              <span class="card-title Subtitulos"></span>
-              <i class="center"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"
-                width="50" height="50"
-                viewBox="0 0 172 172"
-                style=" fill:#000000;"><g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal"><path d="M0,172v-172h172v172z" fill="none"></path><g fill="#000000"><path d="M86,6.88c-43.65844,0 -79.12,35.46156 -79.12,79.12c0,43.65844 35.46156,79.12 79.12,79.12c43.65844,0 79.12,-35.46156 79.12,-79.12c0,-43.65844 -35.46156,-79.12 -79.12,-79.12zM86,13.76c39.93625,0 72.24,32.30375 72.24,72.24c0,39.93625 -32.30375,72.24 -72.24,72.24c-39.93625,0 -72.24,-32.30375 -72.24,-72.24c0,-39.93625 32.30375,-72.24 72.24,-72.24zM75.3575,48.0525c-0.14781,0.02688 -0.29562,0.06719 -0.43,0.1075c-1.29,0.22844 -2.32469,1.16906 -2.6875,2.41875c-0.36281,1.26313 0.01344,2.60688 0.9675,3.49375l31.9275,31.9275l-31.9275,31.9275c-1.37062,1.37063 -1.37062,3.57438 0,4.945c1.37063,1.37063 3.57438,1.37063 4.945,0l34.4,-34.4c0.67188,-0.645 1.04813,-1.54531 1.04813,-2.4725c0,-0.92719 -0.37625,-1.8275 -1.04813,-2.4725l-34.4,-34.4c-0.71219,-0.76594 -1.74687,-1.15562 -2.795,-1.075z"></path></g></g></svg>
-              </i>
-              </div>
-        </div>
-      </a>       
-    </div>
   </section>  
 
 
