033473ec56d04ee62085d92e93ef99a3c5aeb54a
Angelf2902 <81652492+Angelf2902@users.noreply.github.com>
2021-06-14 11:40:25 -0600

    Cambio credenciales

--- California/app/helpers/database.php
+++ California/app/helpers/database.php
@@ -16,9 +16,9 @@ class Database
     {
         // Credenciales para establecer la conexión con la base de datos.
         $server = 'localhost';
-        $database = 'Alexis';
+        $database = 'California';
         $username = 'postgres';
-        $password = '2002';
+        $password = '123';
 
         // Se crea la conexión mediante la extensión PDO y el controlador para PostgreSQL.
         self::$connection = new PDO('pgsql:host='.$server.';dbname='.$database.';port=5432', $username, $password);
