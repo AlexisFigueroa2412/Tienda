441fc70bd4189a43e0ba4632189c1a652e5ce063
Alepo2412 <onedreamermore24.12@gmail.com>
2021-09-11 15:39:45 -0600

    Inactividad js

--- California/app/controllers/dashboard/account.js
+++ California/app/controllers/dashboard/account.js
@@ -6,6 +6,117 @@
 // Constante para establecer la ruta y parámetros de comunicación con la API.
 const API = '../../app/api/dashboard/usuarios.php?action=';
 
+
+//Se activa el contador para Cerrar Sesión en caso de inactividad
+document.addEventListener('DOMContentLoaded', function () {
+    //Se manda a llamar al contador de tiempo
+    contadorInactividad();
+});
+
+//Función que Cierra Sesión en función del tiempo de sesión
+function inactividad() {
+    //Se pide verificar primero si existe una sesión previa
+    fetch(API + 'viewSession', {
+        method: 'get'
+    }).then(function (request) {
+        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+        if (request.ok) {
+            request.json().then(function (response) {
+                // Si existe una sesión previa, se pocede a cerrar la misma
+                // Si no existe una sesión previa, se reinicia el contador
+                if (response.status) {
+                    // Se pide cerrar la sesión
+                    fetch(API + 'logOut', {
+                        method: 'get'
+                    }).then(function (request) {
+                        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+                        if (request.ok) {
+                            request.json().then(function (response) {
+                                // Se comprueba si la sesión se ha cerrado correctamente, de lo contrario se muestra un mensaje con la excepción.
+                                if (response.status) {
+                                    sweetAlert(4, 'Se ha cerrado sesión por inactividad', 'index.php');
+                                } else {
+                                    sweetAlert(2, response.exception, null);
+                                }
+                            });
+                        } else {
+                            console.log(request.status + ' ' + request.statusText);
+                        }
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+                } else {
+                    if(t) clearTimeout(t);
+                    contadorInactividad();
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+// Variable que maneja el parámetro de cierre del contador
+var t=null;
+
+// Función que ejecuta el contador 
+function contadorInactividad() {
+    //Se inicializa el contador a 5 minutos para ejecutar la función "inactividad" que obliga al cierre de sesión
+    t=setTimeout("inactividad()",300000);
+}
+
+// Se verifica si existe actividad en el sitio
+window.onblur=window.onmousemove=function() {
+    // Al haber actividad se pide verificar si existe un sesión activa
+    fetch(API + 'viewSession', {
+        method: 'get'
+    }).then(function (request) {
+        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+        if (request.ok) {
+            request.json().then(function (response) {
+                // Se comprueba si existe una sesión activa, de lo contrario se muestra un mensaje con la excepción.
+                // Si no existe una sesión previa, se reinicia el contador
+                if (response.status) {
+                    // Se pide que se actualice el estado del tiempo de la sesión
+                    fetch(API + 'controlTime', {
+                        method: 'get'
+                    }).then(function (request) {
+                        // Se verifica si la petición es correcta, de lo contrario se muestra un mensaje indicando el problema.
+                        if (request.ok) {
+                            request.json().then(function (response) {
+                                // Se comprueba si el estado del tiempo se actualizo, de lo contrario se muestra un mensaje con la excepción.
+                                if (response.status) {
+                                    //Se reinicia el contador
+                                    if(t) clearTimeout(t);
+                                    contadorInactividad();
+                                } else {
+                                    sweetAlert(2, response.exception, null);
+                                }
+                            });
+                        } else {
+                            console.log(request.status + ' ' + request.statusText);
+                        }
+                    }).catch(function (error) {
+                        console.log(error);
+                    });
+                    
+                } else {
+                    if(t) clearTimeout(t);
+                    contadorInactividad();
+                }
+            });
+        } else {
+            console.log(request.status + ' ' + request.statusText);
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
+
+
+
 // Función para mostrar el formulario de editar perfil con los datos del usuario que ha iniciado sesión.
 function openProfileDialog() {
     // Se abre la caja de dialogo (modal) que contiene el formulario para editar perfil, ubicado en el archivo de las plantillas.
