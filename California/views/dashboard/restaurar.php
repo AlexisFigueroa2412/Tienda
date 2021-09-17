d0f17b6343fd8e3ab28a1bdaefa957aff289411e
danielcarcamoo <daniel.delgado.carcamo@gmail.com>
2021-09-16 18:49:43 -0600

    vista nueva de la restauracion de clave

--- California/views/dashboard/restaurar.php
+++ California/views/dashboard/restaurar.php
@@ -1,24 +1,90 @@
 <?php
+include("../../app/helpers/private_page.php");
+Dashboard_Page::headerTemplate('California','California');
 ?>
 
-<section>
-<div class="container z-depth-1 hide-on-small-only" id="container">
-		<div class="form-container sign-up-container">
-			<form action="#">
-				<a class="btn white z-depth-0 black-text modal-trigger" data-target="terminos">Al registrarte estas
-					aceptando los terminos y condiciones</a>
-				<a class="btn black waves-effect waves-light hoverable" href="#!">Registrarme</a>
-			</form>
-		</div>
-		<div class="form-container sign-in-container">
-			<form method="POST" id="session-form" action="#">
-				<h3 class="Titulos black-text">Iniciar Sesion</h3>
-				<span>Ingresa tus credenciales</span>
-				<input id="alias" name="alias" type="text" placeholder="Usuario" autocomplete="off" class="validate" required/>
-				<input id="clave" name="clave" type="password" placeholder="Contraseña" autocomplete="off" class="validate" required/>
-				<button type="submit" class="btn black waves-effect waves-light hoverable">Iniciar Sesión</button><br>
-				<a class="Texto link" href="index.php">Sitio Público</a>
-				<a class="Texto link" href="restaurar.php">¿Olvidaste tu contraseña?</a>
-			</form>
-		</div>
-</section>
+<!--Contenedor para mostrar el div contenedor a la card en la cual se muestra:-->
+<!--el formulario de recuperar contraseña.-->
+<br>
+<br>
+<br>
+<div class="row container" id="ocultable">
+    <div class="col s12">
+        <div class="card white rad">
+             <!--Defiendo el contenido de la card que contendrá el formulario-->
+            <div class="card-content black-text">
+                <a class="waves-effect black darken-3 white-text btn" href="../views/index.php">
+                    <i class="material-icons left">arrow_back</i>Login
+                </a>
+                <br>
+                <br>
+                <!--Colocamos el titulo de la card-->
+                <span class="card-title center-align">Recuperación de contraseña</span>
+                <br>
+                <!--Estableciendo el tamaño de cada div correspondiente-->
+                <!--Creamos la estructura del formulario respectivo-->
+                <div class="row">
+                    <form class="col-md-4">
+                        <div class="row">
+                            <!--Estableciendo el tamaño del que tomará el Input field-->
+                            <div class="input-field col s12 m6">
+                                <input id="usuario" type="text" class="validate">
+                                <label for="usuario">Ingrese su usuario:</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <!--Estableciendo el tamaño del que tomará el Input field-->
+                            <div class="input-field col s12 m6">
+                                <button class="btn waves-effect black darken-3 white-text" type="submit" name="action">Enviar código
+                                    <i class="material-icons right">send</i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <!--Estableciendo el tamaño del que tomará el Input field-->
+                            <div class="input-field col s12">
+                                <p class="justificado">Se ha enviado un código de confirmación al correo electrónico
+                                    asociado a esta cuenta, por favor revisa tu bandeja de entrada y procede a cambiar
+                                    tu contraseña.
+                                </p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <!--Estableciendo el tamaño del que tomará el Input field-->
+                            <div class="input-field col s12 m6">
+                                <input id="usuario" type="text" class="validate">
+                                <label for="usuario">Ingrese el código de confirmación:</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <!--Estableciendo el tamaño del que tomará el Input field-->
+                            <div class="input-field col s12 m6">
+                                <input id="usuario" type="text" class="validate">
+                                <label for="usuario">Nueva contraseña:</label>
+                            </div>
+                            <!--Estableciendo el tamaño del que tomará el Input field-->
+                            <div class="input-field col s12 m6">
+                                <input id="usuario" type="text" class="validate">
+                                <label for="usuario">Confirmar contraseña:</label>
+                            </div>
+                            <!--Estableciendo el tamaño del que tomará el Input field-->
+                            <div class="input-field col s12 m6">
+                                <button class="btn waves-effect black accent-2" type="submit" name="action">Cambiar
+                                    contraseña
+                                    <i class="material-icons right">edit</i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<br>
+<?php
+  //Se imprime la plantilla del pie y se envía el nombre del controlador para la página web
+  Dashboard_Page::footerTemplate('main.js');
+?>
